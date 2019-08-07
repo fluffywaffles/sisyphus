d984071ab686ad51f007e17a6cd62d909e51115b
fluffywaffles <skorlir@gmail.com>
2019-08-06 21:57:44 -0500

    Infra+Test: add very basic benchmarking to sanity check

--- sanity/check.mjs
+++ sanity/check.mjs
@@ -1,4 +1,4 @@
-import { all, each } from '@prettybad/util'
+import { all, map, each } from '@prettybad/util'
 import sisyphus, { stateful, verisimilitude } from '../lib/index'
 
 const { ok, eq, refeq } = verisimilitude
@@ -9,7 +9,11 @@ const some_obj = { a: 5 }
     , f = function () {}
 
 each((assert, i) => {
-  if (!assert()) console.error('failure!', i, assert)
+  const start_time = process.hrtime()
+  const result = assert()
+  const [ s, ns ] = process.hrtime(start_time)
+  console.log(`${s}s ${ns/1e6}ms`, `\t${result ? '✓' : '✗'}\t`, assert.toString())
+  if (!result) console.error('failure!', i, assert)
 })([
   _ => ok(true),
   _ => !ok(false),
@@ -48,9 +52,19 @@ each((assert, i) => {
   _ => !eq([ { a: { set b (v) { } } } ])([ { a: { get b ( ) { } } } ]),
 ])
 
-const suite = sisyphus(stateful)
+const suite = sisyphus()
 
-const results = suite(`things are as they appear`, [
+function time_t (test) {
+  return t => {
+    const start_time = process.hrtime()
+    const result = test(t)
+    const [ s, ns ] = process.hrtime(start_time)
+    console.log(`${s}s ${ns/1e6}ms`, `\t${result ? '✓' : '✗'}\t`, test.toString())
+    return result
+  }
+}
+
+const results = suite(`things are as they appear`, map(time_t)([
   t => t.ok(true),
   t => !t.ok(false),
   t => t.eq(5)(5),
@@ -58,6 +72,6 @@ const results = suite(`things are as they appear`, [
   t => !t.eq({ get a () { return 7 } })({ get a () { return 7 } }),
   t => t.refeq(5)(5),
   t => t.eq({ a: 5, b: 3 })({ b: 3, a: 5 }),
-])
+]))
 
 all(v => v === true)(results) || (_ => { throw new Error(`test failure`) })()
