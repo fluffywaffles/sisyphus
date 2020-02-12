ee6fef7f9a09b286f0d0508670f037ae4a5b7655
fluffywaffles <skorlir@gmail.com>
2020-02-11 23:11:17 -0500

    Test+Refactor: use @prettybad/util time-profiler to time tests

--- sanity/check.mjs
+++ sanity/check.mjs
@@ -8,7 +8,9 @@ import {
   get,
   set,
   map,
+  not,
   each,
+  time,
   reflex,
   flatten,
 } from '@prettybad/util'
@@ -24,78 +26,64 @@ const some_obj = { a: 5 }
 {
   const { ok, eq, refeq } = verisimilitude
   console.log(`\n>> begin asserts\n`)
-  const start_time = process.hrtime()
-  each((assert, i) => {
-    const start_time = process.hrtime()
-    const result = assert()
-    const [ s, ns ] = process.hrtime(start_time)
-    console.log(`${s}s\t${ns/1e6}ms`, `\t${result ? '✓' : '✗'}\t`, assert.toString())
-    if (result !== true) {
-      console.error('failure!', i, assert)
-    }
-  })([
-    _ => ok(true),
-    _ => !ok(false),
-    _ => refeq(some_obj)(some_obj),
-    _ => !refeq(some_obj)({ a: 5 }),
-    _ => eq(some_obj)(some_obj),
-    _ => eq(some_obj)({ a: 5 }),
-    _ => eq('a')('a'),
-    _ => eq(false)(false),
-    _ => eq(true)(true),
-    _ => eq(5)(5),
-    _ => eq(Infinity)(Infinity),
-    _ => eq(-Infinity)(-Infinity),
-    _ => !eq(Infinity)(-Infinity),
-    _ => eq()(),
-    _ => eq(undefined)(undefined),
-    _ => eq(null)(null),
-    _ => eq({})({}),
-    _ => eq([])([]),
-    _ => eq([{},{}])([{},{}]),
-    _ => eq([[[]]])([[[]]]),
-    _ => eq(f)(f),
-    _ => !eq(function () {})(function () {}),
-    _ => eq(NaN)(NaN),
-    _ => !eq([ 1, 2, 3 ])([ 1, 2, 4 ]),
-    _ => !eq([ [ 1 ], 2, 3 ])([ [ 2 ], 2, 3 ]),
-    _ => !eq({ a: 5 })({ b: 5 }),
-    _ => eq([ 1, 2, 3 ])([ 1, 2, 3 ]),
-    _ => eq({ a: 5 })({ a: 5 }),
-    _ => eq({ a: { b: 5 }  })({ a: { b: 5 } }),
-    _ => !eq({ a: 5  })({ get a() { return 5 }}),
-    // getters and setters must be refeq to be eq
-    _ => eq(get_set)(get_set),
-    _ => !eq({ get a () {  } })({ get a () {  } }),
-    _ => !eq(get_set)({ get a () { return 5 }, set a (v) { /* side-effect */ } }),
-    _ => !eq([ { a: { set b (v) {} } } ])([ { a: { get b () {} } } ]),
-    _ => eq({ a: 5, b: 3 })({ b: 3, a: 5 }),
-  ])
-  const [ s, ns ] = process.hrtime(start_time)
-  console.log(`${s}s\t${ns/1e6}ms`, `\ttotal`)
+  const profile = time(() => {
+    each((assert, i) => {
+      const profile = time(assert)
+      console.log(
+        profile.summarize({ resolution: 'us' }),
+        `\t${profile.value ? '✓' : '✗'}\t`,
+        assert.toString(),
+      )
+      if (not(is(true))(profile.value)) {
+        console.error('failure!', i, assert)
+      }
+    })([
+      _ => ok(true),
+      _ => !ok(false),
+      _ => refeq(some_obj)(some_obj),
+      _ => !refeq(some_obj)({ a: 5 }),
+      _ => eq(some_obj)(some_obj),
+      _ => eq(some_obj)({ a: 5 }),
+      _ => eq('a')('a'),
+      _ => eq(false)(false),
+      _ => eq(true)(true),
+      _ => eq(5)(5),
+      _ => eq(Infinity)(Infinity),
+      _ => eq(-Infinity)(-Infinity),
+      _ => !eq(Infinity)(-Infinity),
+      _ => eq()(),
+      _ => eq(undefined)(undefined),
+      _ => eq(null)(null),
+      _ => eq({})({}),
+      _ => eq([])([]),
+      _ => eq([{},{}])([{},{}]),
+      _ => eq([[[]]])([[[]]]),
+      _ => eq(f)(f),
+      _ => !eq(function () {})(function () {}),
+      _ => eq(NaN)(NaN),
+      _ => !eq([ 1, 2, 3 ])([ 1, 2, 4 ]),
+      _ => !eq([ [ 1 ], 2, 3 ])([ [ 2 ], 2, 3 ]),
+      _ => !eq({ a: 5 })({ b: 5 }),
+      _ => eq([ 1, 2, 3 ])([ 1, 2, 3 ]),
+      _ => eq({ a: 5 })({ a: 5 }),
+      _ => eq({ a: { b: 5 }  })({ a: { b: 5 } }),
+      _ => !eq({ a: 5  })({ get a() { return 5 }}),
+      // getters and setters must be refeq to be eq
+      _ => eq(get_set)(get_set),
+      _ => !eq({ get a () {  } })({ get a () {  } }),
+      _ => !eq(get_set)({ get a () { return 5 }, set a (v) { /* side-effect */ } }),
+      _ => !eq([ { a: { set b (v) {} } } ])([ { a: { get b () {} } } ]),
+      _ => eq({ a: 5, b: 3 })({ b: 3, a: 5 }),
+    ])
+  })
+  console.log(profile.summarize({ resolution: 'us' }), `\ttotal`)
   console.log(`\n<< end asserts`)
 }
 
 {
   const harness = sisyphus({ reporter: reporters.simple })
-
-  function time_t (test) {
-    return set.values.mut({
-      toString () {
-        return test.toString()
-      },
-    })(t => {
-      const start_time = process.hrtime()
-      const result = test(t)
-      const [ s, ns ] = process.hrtime(start_time)
-      console.log(`${s}s ${ns/1e6}ms`)
-      return result
-    })
-  }
-
   console.log(`\n>> begin suite\n`)
-  const start_time = process.hrtime()
-  const results = harness.suite(`things are as they appear`, [
+  const profile = time(() => harness.suite(`things are as they appear`, [
     t => t.ok(true),
     t => !t.ok(false),
     t => t.eq(5)(5),
@@ -111,13 +99,12 @@ const some_obj = { a: 5 }
       'sub-sub-suite a': [ t => t.ok(true) ],
       'sub-sub-suite b': [ t => t.ok(true) ],
     }),
-  ])
-  const [ s, ns ] = process.hrtime(start_time)
-  console.log(`${s}s ${ns/1e6}ms`, `\tsuite`)
+  ]))
+  console.log(profile.summarize({ resolution: 'us' }), `\tsuite`)
 
   const deep_flatten = messages =>
     ᐅwhen(any(reflex.instance.Array))(ᐅ([ flatten, deep_flatten ]))
-  if (!all(is(true))(deep_flatten(results))) {
+  if (!all(is(true))(deep_flatten(profile.value))) {
     throw new Error(`test failure`)
   }
   console.log(`\n<< end suite`)
